8ada370368c819f31f9e3a2bed858199441ce209
Koss Soni <kosssoni@school21.ru>
2026-03-08 14:39:07 +0300

    add description and enums to BasicGroup enum

--- Ozon/Analytics/Enums/BasicGroup.cs
+++ Ozon/Analytics/Enums/BasicGroup.cs
@@ -1,6 +1,37 @@
+using System.Text.Json.Serialization;
+
 namespace Ozon.Analytics.Enums;
 
-public enum BasicGroup
+public enum BasicGroup:int
 {
-    
+    /// <summary>
+    /// Неизвестное измерение
+    /// </summary>
+    [JsonPropertyName("unknownDimension")]
+    UnknownDimension ,
+    /// <summary>
+    /// Идентификатор товара,
+    /// </summary>
+    [JsonPropertyName("sku")]
+    Sku ,
+    /// <summary>
+    /// Идентификатор товара — объединённая карточка
+    /// </summary>
+    [JsonPropertyName("spu")]
+    Spu ,
+    /// <summary>
+    /// День
+    /// </summary>
+    [JsonPropertyName("day")]
+    Day,
+    /// <summary>
+    ///  Неделя
+    /// </summary>
+    [JsonPropertyName("week")]
+    Week,
+    /// <summary>
+    /// Месяц
+    /// </summary>
+    [JsonPropertyName("month")]
+    Month
 }
