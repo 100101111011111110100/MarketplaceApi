fcca6d4ba25c313e656c22cc7de3391d049e1181
Koss Soni <kosssoni@school21.ru>
2026-03-08 16:40:30 +0300

    add extension dir and extension for enum

--- Ozon/Extensions/EnumExtensions.cs
+++ Ozon/Extensions/EnumExtensions.cs
@@ -1,6 +1,20 @@
+using System.Runtime.Serialization;
 namespace Ozon.Extensions;
 
-public class EnumExtensions
+public static class EnumExtensions
 {
-    
+    public static string ToApiString(this Enum value)
+    {
+        var type = value.GetType();
+        var member = type.GetMember(value.ToString());
+
+        if (member.Length > 0)
+        {
+            var attr = member[0].GetCustomAttribute<EnumMemberAttribute>();
+            if (attr != null)
+                return attr.Value;
+        }
+
+        return value.ToString();
+    }
 }
