c87cdfc19e62992e3a67e4f5195e420326254d49
Koss Soni <kosssoni@school21.ru>
2026-03-08 14:41:37 +0300

    add enums to Premium Group enum and description

--- Ozon/Analytics/Enums/PremiumGroup.cs
+++ Ozon/Analytics/Enums/PremiumGroup.cs
@@ -1,6 +1,42 @@
+using System.Text.Json.Serialization;
+
 namespace Ozon.Analytics.Enums;
 
-public enum PremiumGroup
+public enum PremiumGroup :int
 {
-    
+    /// <summary>
+    ///  Год
+    /// </summary>
+    [JsonPropertyName("year")]
+    Year,
+    /// <summary>
+    /// Категория первого уровня
+    /// </summary>
+    [JsonPropertyName("category1")]
+    Category1,
+    /// <summary>
+    /// Категория второго уровня
+    /// </summary>
+    [JsonPropertyName("")]
+    Category2,
+    /// <summary>
+    /// Категория третьего уровня
+    /// </summary>
+    [JsonPropertyName("category3")]
+    Category3,
+    /// <summary>
+    /// Категория четвертого уровня
+    /// </summary>
+    [JsonPropertyName("category4")]
+    Category4,
+    /// <summary>
+    /// Бренд
+    /// </summary>
+    [JsonPropertyName("brand")]
+    Brand,
+    /// <summary>
+    /// Модель
+    /// </summary>
+    [JsonPropertyName("modelID")]
+    ModelID
 }
